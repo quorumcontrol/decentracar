ca55b3272877adc77466c8a53bed59cc23ea748c
Topper Bowers <topper@toppingdesign.com>
2019-09-19 22:18:03 +0200

    switch to production in production

--- src/decentracar/util/appcommunity.ts
+++ src/decentracar/util/appcommunity.ts
@@ -1,19 +1,26 @@
 
 import { Community } from 'tupelo-wasm-sdk';
 import debug from 'debug';
+import process from 'process'
 
 const log = debug("appcommunity")
 
-let _appPromise:Promise<Community>
+let _appPromise: Promise<Community>
 
 export function getAppCommunity(): Promise<Community> {
-   log("getAppCommunity")
-   if (_appPromise !== undefined) {
-       return _appPromise
-   }
-    _appPromise = new Promise(async (resolve,reject)=> {
-        // const c = await Community.freshLocalTestCommunity()
-        const c = await Community.getDefault()
+    log("getAppCommunity")
+    if (_appPromise !== undefined) {
+        return _appPromise
+    }
+    _appPromise = new Promise(async (resolve, reject) => {
+        let c: Community
+        switch (process.env.NODE_ENV) {
+            case 'production':
+                c = await Community.getDefault()
+                break;
+            default:
+                c = await Community.freshLocalTestCommunity()
+        }
         resolve(c)
     })
     return _appPromise
